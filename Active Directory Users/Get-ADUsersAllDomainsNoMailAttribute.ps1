58a0c7016f66cdc09cd4a67159ea34f41a6a5461
davevanvleet <dave.vanvleet@yahoo.com>
2023-09-11 13:16:49 -0500

    First Commit

---
+++ Active Directory Users/Get-ADUsersAllDomainsNoMailAttribute.ps1
@@ -0,0 +1,4 @@
+﻿# Provide the file export path and filename.
+$export = Read-Host
+# Get all AD Users in Forest that DO NOT have a mail attribute.
+(Get-ADForest).Domains | % {Get-ADUser -filter * -Server $_ -Properties * | where-object {$_.Mail -ne $null} | Select SamAccountName, Name, Mail, DistinguishedName, UserPrincipalName} | Export-csv $export -NoTypeInformation -Append
